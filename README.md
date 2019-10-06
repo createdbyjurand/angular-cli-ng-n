65d104f500075881e8d28ee40fafc07360f797b6
createdbyjurand <createdbyjurand@gmail.com>
2019-10-06 22:08:36 +0200

    initial commit ( @angular/cli@8.3.7 )

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularCliNgNRoutingTrueStyleScss
+# AngularCliNgNStyleScss
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.6.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.7.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-cli-ng-n-routing-true-style-scss'`, () => {
+  it(`should have as title 'angular-cli-ng-n-style-scss'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-cli-ng-n-routing-true-style-scss');
+    expect(app.title).toEqual('angular-cli-ng-n-style-scss');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-cli-ng-n-routing-true-style-scss app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-cli-ng-n-style-scss app is running!');
   });
 });
